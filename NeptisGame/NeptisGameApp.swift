a344839501473f2d69208003a6b0fee72a7cb8ea
Harun Begic <hbegiic1@gmail.com>
2024-07-29 23:24:55 +0200

    Initial Commit

---
+++ NeptisGame/NeptisGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeptisGameApp.swift
+//  NeptisGame
+//
+//  Created by Harun Begic on 29. 7. 2024..
+//
+
+import SwiftUI
+
+@main
+struct NeptisGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
